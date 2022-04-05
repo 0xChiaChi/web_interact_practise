6603d723d33f1ad59ac46e5b7134ba0ef8f7ae56
0xChiaChi <king565634@gmail.com>
2022-04-05 21:39:49 +0800

    Update create_img.js

--- js/create_img.js
+++ js/create_img.js
@@ -9,16 +9,16 @@ window.onload=function () {
 			console.log(jsonData);
 			var area = document.getElementById("area");
 			$.each(jsonData, function (index, value) { 
-				let x=((Math.random() *20)+5);
-				let y=((Math.random() *40));
+				let x=((Math.random() *50)+5);
+				let y=((Math.random() *60)+5);
 				let area_x=area.offsetWidth/100;
 			    x*=area_x;
 			    let area_y=area.offsetHeight/100;
 			    y*=area_y;
 			    var div = document.createElement("div");
 			    div.className =" drag-drop artwork";
-			    div.style ="height:200px;padding:0;transform:translate("+x+"px, "+y+"px);";
-			    div.innerHTML = '<img src="collection/'+value['圖片名稱']+'"class="w-100 h-100" style="object-fit: contain;" value="'+value['價格']+'">';
+			    div.style ="height:200px;padding:0;transform:translate("+x+"px, "+y+"px); max-width:60%";
+			    div.innerHTML = '<img src="collection/'+value['圖片名稱']+'" class="w-100 h-100" style="object-fit: contain;" value="'+value['價格']+'">';
 			    area.appendChild(div);
 			 //    var x = 0;
 				// var y = 0;
@@ -29,16 +29,14 @@ window.onload=function () {
 				//   // y*=0.5;
 
 				// div.style.webkitTransform=div.style.transform="translate(" + x + "px, " + y + "px)";
-				console.log("1"+$(div).offset().top);
-				console.log("2"+$(div).offset().bottom);
-				console.log("3"+$(div).offset().left);
-				console.log("4"+$(div).offset().right);
-				if($(div).offset().bottom>area.offsetHeight/2||$(div).offset().top>area.offsetHeight/2){
-					y*=0.1;
-				}
-				if($(div).offset().left>area.offsetWidth/2||$(div).offset().right>area.offsetWidth/2){
-					x*=0.1;
-				}
+				// console.log("1"+$(div).offset().top);
+				// console.log("3"+$(div).offset().left);
+				// if($(div).offset().top>area.offsetHeight){
+				// 	y-=(y-area.offsetHeight);
+				// }
+				// if($(div).offset().left>area.offsetWidth){
+				// 	x-=(x-area.offsetWidth);
+				// }
 			   	div.setAttribute("data-x", x);
 			    div.setAttribute("data-y", y);
 			})
